a0bddbbbf8b26a335454a36c88d3d4ad9245af90
David Wheatley <david@davwheat.dev>
2023-10-06 11:42:26 +0100

    chore: format

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/utils/ExecuteAsRoot.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/utils/ExecuteAsRoot.kt
@@ -1,10 +1,10 @@
 package dev.davwheat.shannonmodemtweaks.utils
 
+import timber.log.Timber
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.IOException
 import java.io.InputStreamReader
-import timber.log.Timber
 
 object ExecuteAsRoot {
   fun hasRoot(): Boolean {
