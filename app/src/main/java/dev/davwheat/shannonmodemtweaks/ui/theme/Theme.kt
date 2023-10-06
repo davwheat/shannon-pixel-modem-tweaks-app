96d5263377d8d4874ecf25d77fe6dbf34f55bdc1
David Wheatley <david@davwheat.dev>
2023-10-06 02:51:29 +0100

    chore: format

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/ui/theme/Theme.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/ui/theme/Theme.kt
@@ -14,44 +14,40 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-  primary = Purple80,
-  secondary = PurpleGrey80,
-  tertiary = Pink80
-)
+private val DarkColorScheme =
+    darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
 
-private val LightColorScheme = lightColorScheme(
-  primary = Purple40,
-  secondary = PurpleGrey40,
-  tertiary = Pink40
+private val LightColorScheme =
+    lightColorScheme(
+        primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
 
-  /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+        /* Other default colors to override
+        background = Color(0xFFFFFBFE),
+        surface = Color(0xFFFFFBFE),
+        onPrimary = Color.White,
+        onSecondary = Color.White,
+        onTertiary = Color.White,
+        onBackground = Color(0xFF1C1B1F),
+        onSurface = Color(0xFF1C1B1F),
+        */
+        )
 
 @Composable
 fun ShannonModemTweaksTheme(
-  darkTheme: Boolean = isSystemInDarkTheme(),
-  // Dynamic color is available on Android 12+
-  dynamicColor: Boolean = true,
-  content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
-  val colorScheme = when {
-    dynamicColor -> {
-      val context = LocalContext.current
-      if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-    }
-
-    darkTheme -> DarkColorScheme
-    else -> LightColorScheme
-  }
+  val colorScheme =
+      when {
+        dynamicColor -> {
+          val context = LocalContext.current
+          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+      }
   val view = LocalView.current
   if (!view.isInEditMode) {
     SideEffect {
@@ -61,9 +57,5 @@ fun ShannonModemTweaksTheme(
     }
   }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = Typography,
-    content = content
-  )
+  MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
 }
