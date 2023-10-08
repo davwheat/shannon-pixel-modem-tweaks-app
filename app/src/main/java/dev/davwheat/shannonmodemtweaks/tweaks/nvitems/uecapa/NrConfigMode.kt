4f74ad198562ca8472a50e4799cae6716341a44b
David Wheatley <david@davwheat.dev>
2023-10-09 00:43:36 +0100

    chore: change nr config mode name/description

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/uecapa/NrConfigMode.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/uecapa/NrConfigMode.kt
@@ -4,12 +4,13 @@ import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.NvItem
 import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.NvItemTweak
 
 class NrConfigMode : NvItemTweak() {
-  override val name = "NR config mode"
-  override val description = "Enable NR NSA and SA modes."
+  override val name = "Enable NSA and SA 5G modes"
+  override val description = "Applies to both SIMs"
 
   override val nvItems: List<NvItem>
     get() =
         listOf(
+            // Sets allowed NR modes. 00=disabled, 01=NSA, 10=SA, 11=SA+NSA
             NvItem(id = "NR.CONFIG.MODE", value = "11"),
             NvItem(id = "DS.NR.CONFIG.MODE", value = "11"),
         )
