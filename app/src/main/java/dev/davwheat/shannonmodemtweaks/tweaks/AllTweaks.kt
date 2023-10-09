f9ce0828dd90360807d6f533b3712d654c9c476c
Josh Hill <ingenium@gmail.com>
2023-10-09 13:38:15 -0400

    Add HPUE PC 1.5

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/AllTweaks.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/AllTweaks.kt
@@ -5,6 +5,7 @@ import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.LteRrcLoggedMeas
 import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.NrConfigMode
 import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.uecapa.PhyUeSpecificRefSigSupport
 import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.uecapa.PhyUeTxAntennaSelectionSupport
+import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.HpuePC15
 
 val AllTweaks: Map<String, List<Tweak>> =
     mapOf(
@@ -16,6 +17,7 @@ val AllTweaks: Map<String, List<Tweak>> =
                 NrConfigMode(),
                 PhyUeSpecificRefSigSupport(),
                 AdditionalNrSaLocking(),
+		HpuePC15(),
             ).sortedBy { it.name },
         ),
     )
---
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/HpuePC15.kt
@@ -0,0 +1,30 @@
+package dev.davwheat.shannonmodemtweaks.tweaks.nvitems
+
+import dev.davwheat.shannonmodemtweaks.utils.toNvItemHexString
+
+class HpuePC15 : NvItemTweak() {
+  override val name = "Enable HPUE PC 1.5"
+  override val description = "Enables HPUE"
+
+  private val bands = listOf(41, 77)
+
+  override val nvItems: List<NvItem>
+    get() =
+        listOf(
+            *bands
+                .mapIndexed { index, band ->
+                  NvItem(
+                      id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS",
+                      index = index,
+                      value = band.toNvItemHexString(2),
+                  )
+                }
+                .toTypedArray(),
+
+            // Bands count
+            NvItem(
+                id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS_NUM",
+                value = bands.size.toNvItemHexString(2),
+            ),
+        )
+}
