9b79f64b1af4b15d57717de30f858a955e3b9bb1
David Wheatley <david.wheatley@raileasy.co.uk>
2023-10-12 00:21:04 +0100

    fix: broken srs antenna switching

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/nrcapa/SrsTxSwitch.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/nrcapa/SrsTxSwitch.kt
@@ -6,11 +6,11 @@ import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.NvItemTweak
 class SrsTxSwitch : NvItemTweak() {
   override val name = "SRS Tx antenna switching"
   override val description =
-      "Allows the device to perform network-controlled cell signal measurements for analytics."
+      "Device can transmit reference signals on Rx antenna chain to improve DL performance."
 
   override val nvItems: List<NvItem>
     get() =
         listOf(
-            NvItem(id = "!LTEL3.LTERRC_LOGGED_MEAS", value = "01"),
+            NvItem(id = "!NRCAPA.Gen.SrsTxSwitch", value = "01"),
         )
 }
