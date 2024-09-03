9fcccad253a8f720458238045eae3707c12852cd
David Wheatley <david@davwheat.dev>
2024-09-03 21:51:24 +0100

    feat: support Pixel 8a

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/utils/InferDevice.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/utils/InferDevice.kt
@@ -63,6 +63,7 @@ object InferDevice {
     PIXEL_FOLD("felix", "Google Pixel Fold"),
     PIXEL_8("shiba", "Google Pixel 8"),
     PIXEL_8_PRO("husky", "Google Pixel 8 Pro"),
+    PIXEL_8A("akita", "Google Pixel 8a"),
     PIXEL_9("tokay", "Google Pixel 9"),
     PIXEL_9_PRO("caiman", "Google Pixel 9 Pro"),
     PIXEL_9_PRO_XL("komodo", "Google Pixel 9 Pro XL"),
