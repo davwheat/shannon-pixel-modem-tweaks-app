d91e6d921f6090a19857c69539764abb361906a5
handymenny <handymenny@outlook.com>
2024-11-22 20:30:26 +0100

    rrc inactive: limit compatibility to pixel 9 series

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/nrcapa/Rrcinactive.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/nrcapa/Rrcinactive.kt
@@ -3,6 +3,7 @@ package dev.davwheat.shannonmodemtweaks.tweaks.nvitems.nrcapa
 import android.os.Parcelable
 import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.NvItem
 import dev.davwheat.shannonmodemtweaks.tweaks.nvitems.NvItemTweak
+import dev.davwheat.shannonmodemtweaks.utils.InferDevice.PixelDevice
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
@@ -19,4 +20,14 @@ class Rrcinactive : NvItemTweak(), Parcelable {
             NvItem(id = "!NRCAPA_INACTIVE_STATE", value = "01"),
             NvItem(id = "!NRCAPA_INACTIVE_STATE_DS", value = "01"),
         )
+
+  @IgnoredOnParcel
+  private val compatibleDevices = setOf(
+    PixelDevice.PIXEL_9,
+    PixelDevice.PIXEL_9_PRO,
+    PixelDevice.PIXEL_9_PRO_FOLD,
+    PixelDevice.PIXEL_9_PRO_XL
+  )
+
+  override fun isTweakCompatible(device: PixelDevice): Boolean = device in compatibleDevices
 }
