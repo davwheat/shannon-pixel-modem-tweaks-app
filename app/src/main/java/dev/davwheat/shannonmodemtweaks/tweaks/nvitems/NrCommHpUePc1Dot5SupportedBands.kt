696419b961b1e2b02efe3e4f2e3d28fbe799fb44
Henrik <15855905+high3eam@users.noreply.github.com>
2024-12-14 00:03:52 +0100

    Add n78 band and dual-sim item

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/NrCommHpUePc1Dot5SupportedBands.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/NrCommHpUePc1Dot5SupportedBands.kt
@@ -11,9 +11,9 @@ class NrCommHpUePc1Dot5SupportedBands : NvItemTweak(), Parcelable {
   override val name = "Enable HPUE PC1.5"
 
   @IgnoredOnParcel
-  override val description = "Enables HPUE power class 1.5 for n41/77"
+  override val description = "Enables HPUE power class 1.5 for n41/n77/n78 Dual-SIM"
 
-  private val bands get() = listOf(41, 77)
+  private val bands get() = listOf(41, 77, 78)
 
   override val nvItems: List<NvItem>
     get() =
@@ -22,6 +22,11 @@ class NrCommHpUePc1Dot5SupportedBands : NvItemTweak(), Parcelable {
           .mapIndexed { index, band ->
             NvItem(
               id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS",
+              index = index,
+              value = band.toNvItemHexString(2),
+            ),
+            NvItem(
+              id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS_DS",
               index = index,
               value = band.toNvItemHexString(2),
             )
