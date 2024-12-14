d84ec3c982e5dff0036cea8d30aef6cba66c68aa
Henrik <15855905+high3eam@users.noreply.github.com>
2024-12-14 15:10:58 +0100

    Add n78 band and dual-sim item (#19)
    * Add n78 band and dual-sim item
    
    * Update NrCommHpUePc1Dot5SupportedBands.kt

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/NrCommHpUePc1Dot5SupportedBands.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/NrCommHpUePc1Dot5SupportedBands.kt
@@ -11,27 +11,27 @@ class NrCommHpUePc1Dot5SupportedBands : NvItemTweak(), Parcelable {
   override val name = "Enable HPUE PC1.5"
 
   @IgnoredOnParcel
-  override val description = "Enables HPUE power class 1.5 for n41/77"
+  override val description = "Enables HPUE power class 1.5 for n41/n77/n78 Dual-SIM"
 
-  private val bands get() = listOf(41, 77)
+  private val bands get() = listOf(41, 77, 78)
 
   override val nvItems: List<NvItem>
     get() =
-      listOf(
-        *bands
-          .mapIndexed { index, band ->
-            NvItem(
-              id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS",
-              index = index,
-              value = band.toNvItemHexString(2),
-            )
-          }
-          .toTypedArray(),
-
-        // Bands count
-        NvItem(
-          id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS_NUM",
-          value = bands.size.toNvItemHexString(1),
-        ),
+      bands.flatMapIndexed { index, band ->
+        listOf(
+          NvItem(
+            id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS",
+            index = index,
+            value = band.toNvItemHexString(2)
+          ),
+          NvItem(
+            id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS_DS",
+            index = index,
+            value = band.toNvItemHexString(2)
+          )
+        )
+      } + NvItem(
+        id = "!NRCOMM_PC1DOT5_SUPPORTED_BANDS_NUM",
+        value = bands.size.toNvItemHexString(1)
       )
 }
