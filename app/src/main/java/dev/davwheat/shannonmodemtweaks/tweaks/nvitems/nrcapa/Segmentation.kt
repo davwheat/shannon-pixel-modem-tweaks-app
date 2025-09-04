2478d84826e3380ef7ce572fe6d08121355f79ce
Henrik <15855905+high3eam@users.noreply.github.com>
2025-09-04 13:26:16 +0200

    Add Pixel 10 series to segmentation item

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/nrcapa/Segmentation.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/nrcapa/Segmentation.kt
@@ -28,7 +28,11 @@ class Segmentation : NvItemTweak(), Parcelable {
     PixelDevice.PIXEL_9,
     PixelDevice.PIXEL_9_PRO,
     PixelDevice.PIXEL_9_PRO_FOLD,
-    PixelDevice.PIXEL_9_PRO_XL
+    PixelDevice.PIXEL_9_PRO_XL,
+    PixelDevice.PIXEL_10,
+    PixelDevice.PIXEL_10_PRO,
+    PixelDevice.PIXEL_10_PRO_FOLD,
+    PixelDevice.PIXEL_10_PRO_XL
   )
 
   override fun isTweakCompatible(device: PixelDevice): Boolean = device in compatibleDevices
